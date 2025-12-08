6e96b73329c95590f242fe628d3b753a079c2c08
lraveri <lucaraveri993@gmail.com>
2025-12-08 15:58:29 +0100

    feat: minimal example

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import { useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import './style.css';
 
 const App = () => {
-    return <h1>TS Sandbox!!!</h1>;
+    const [name, setName] = useState('');
+    const [displayedName, setDisplayedName] = useState('');
+
+    const handleSubmit = (): void => {
+        if (!name) return;
+        setDisplayedName(name);
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        setName(event.target.value);
+    };
+
+    const greet = (name: string): string => {
+        return `Hello, ${name}!`;
+    };
+
+    return (
+        <>
+            <h1>React Sandbox</h1>
+            <p>Type your name...</p>
+            <input onChange={handleChange} value={name} type="text" />
+            <button onClick={handleSubmit}>Submit</button>
+            {displayedName && <Card title={greet(displayedName)} content="Lorem ipsum...."></Card>}
+        </>
+    );
+};
+
+type CardProps = {
+    title: string;
+    content: string;
+};
+
+const Card = (props: CardProps) => {
+    return (
+        <div className="card">
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    );
 };
 
 const root = createRoot(document.getElementById('root')!);
